6af3876cc6a0e5979cb5364133f4c5d1ffa840a5
rickyhidayatt <rickyhyouth@gmail.com>
2023-06-24 15:47:34 +0700

    best practice dari time sleep menggunakan wait group

--- main.go
+++ main.go
@@ -2,32 +2,66 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
+func main(){
 
-func main() {
-
-	data := make(chan string)
+	data := make(chan int)
+	err := make(chan string)
 	
-	go sendData(data)
-	go getData(data)
+	wg := new(sync.WaitGroup)
+	wg.Add(2)// menandakan ada 2 gorutine
 
-//  contoh pembuatan go routine langsung dari go routine main
-	go func (in int)  {
-		for i := 0; i < in ; i ++ {
-			fmt.Println(i)
-		} 
-	}(2)
+	go func (w *sync.WaitGroup)  {
+		defer w.Done()
+		for i := 0 ; i < 10 ; i ++{
+			data <- i
+		}
+		err <- "ada err"
 
+	}(wg)
 
-// kenapa ada time sleep ? supaya ga keburu mati program nya jadi nunggu dulu kalo dah beres baru selesai
-	time.Sleep(time.Second)
+	go func (w *sync.WaitGroup)  {
+		defer w.Done()
+		for {
+			select{
+
+			case res := <- err:
+				fmt.Println(res)
+				return
+				
+			case datas := <- data:
+				fmt.Println(datas)
+				
+			}
+		}
+	}(wg)
+// tungguin prosesnya dengan menggunakan wg Wait
+wg.Wait()
+
 }
 
 
+// func main() {
 
-// untuk membuat go routine kita wajib menggunakan type data Chan 
-// karena sistem kerjanya gorutine kaya gini 
+// 	data := make(chan string)
+
+// 	go sendData(data)
+// 	go getData(data)
+
+// //  contoh pembuatan go routine langsung dari go routine main
+// 	go func (in int)  {
+// 		for i := 0; i < in ; i ++ {
+// 			fmt.Println(i)
+// 		}
+// 	}(2)
+
+// // kenapa ada time sleep ? supaya ga keburu mati program nya jadi nunggu dulu kalo dah beres baru selesai
+// 	time.Sleep(time.Second)
+// }
+
+// untuk membuat go routine kita wajib menggunakan type data Chan
+// karena sistem kerjanya gorutine kaya gini
 // kirim data menggunakan go routine -> simpan di channel -> ambil data dari channel ke goroutine lain.
 
 
